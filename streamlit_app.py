0bb55cde3d774670d4c74478edb95807b1a0c44e
dev3000028 <129067814+dev3000028@users.noreply.github.com>
2023-04-18 06:53:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ Third_integer=st.number_input("3rd Integer")
 
 # Check which number is the largest
 if (First_integer >= Second_integer) and (First_integer >= Third_integer):
-    st.write("The largest value  is "+First_integer)
+    st.write("The largest value  is ", First_integer)
 elif (Second_integer >= First_integer) and (Second_integer >= Third_integer):
-    st.write("The largest value is " + Second_integer )
+    st.write("The largest value is " , Second_integer )
 else:
-    st.write("The largest value is " + Third_integer )
+    st.write("The largest value is " ,Third_integer )
